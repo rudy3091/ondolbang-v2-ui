f2571a51ff5cabffc702327951073a06cf4c53e7
rudy3091 <grayblack313@gmail.com>
2021-05-10 00:27:55 +0900

    Update SearchBar component
    검색바 노출을 토글할 수 있도록 변경

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -6,14 +6,26 @@ import { IconHolder } from "@components/IconHolder/IconHolder";
 type SearchBarProps = {};
 
 export const SearchBar: React.FC<SearchBarProps> = (props: SearchBarProps) => {
+	const [isHidden, setHidden] = React.useState<boolean>(true);
+	const className = isHidden
+		? [$.input_container, $.hidden].join(" ")
+		: $.input_container;
+
 	return (
-		<div className={$.container}>
+		<div
+			className={$.container}
+			style={{ backgroundColor: isHidden ? "" : "#f8f8f8" }}
+		>
 			<IconHolder
 				iconPath="/search.svg"
 				width="30px"
 				height="30px"
+				onClick={() => setHidden(!isHidden)}
 			></IconHolder>
-			<input className={$.input} type="text" placeholder="input here"/>
+
+			<div className={className}>
+				<input className={$.input} type="text" />
+			</div>
 		</div>
 	);
 };
